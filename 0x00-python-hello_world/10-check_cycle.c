f108373e8e7a41339584232737c2aaf5589fdf92
Oluwadamilare Odekanmi <oluwadamilareodekanmi@gmail.com>
2023-11-27 16:08:00 -0800

    Linked list cycle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * check_cycle - This is a function that checks if a list has a cycle
+ * @list: This is the linked list
+ * Return: 1 if there is a cycle, otherwise 0
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *fast, *slow;
+	if (!list || !list->next)
+		return (0);
+	fast = list;
+	slow = list;
+
+	while (slow != NULL && fast != NULL && fast->next != NULL)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+			return (1);
+	}
+	return (0);
+}
