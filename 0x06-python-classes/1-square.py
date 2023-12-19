d7655b4f533c24b64ab81b85f38143c2f14d0d8a
Oluwadamilare Odekanmi <oluwadamilareodekanmi@gmail.com>
2023-12-19 07:05:47 -0800

    A script that writes a class Square that defines a square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Define a class Square."""
+
+
+class Square:
+    """This represents a square."""
+
+    def __init__(self, size):
+        """This initializes a new square
+
+        Args:
+            size (int): This is the size of the new square.
+        """
+        self.__size = size
